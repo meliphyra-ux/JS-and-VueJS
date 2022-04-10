745fc4603f6d759cd78b50883721e26af165f2a4
meliphyra-ux <83269567+meliphyra-ux@users.noreply.github.com>
2022-04-10 14:37:47 +0300

    New projects

--- Form Events and RegEx/main.js
+++ Form Events and RegEx/main.js
@@ -2,4 +2,5 @@ const form = document.querySelector('.signup-form');
 form.addEventListener('submit', e =>{
 e.preventDefault();
  console.log(form.username.value)
+ console.log(form.username.value)
 });
